113918084df034e34e72ba414bb1996b381ee6c8
Sudhakar Singh <aryansudhakar09@gmail.com>
2017-03-27 23:59:42 +0530

    Added automatic rssi value saving to csv file.

--- app/src/main/java/com/example/a0132535/wifirssimapper/MainActivity.java
+++ app/src/main/java/com/example/a0132535/wifirssimapper/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.a0132535.wifirssimapper;
 
 import android.Manifest;
 import android.app.Dialog;
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.ContentValues;
 import android.content.Context;
@@ -10,25 +11,35 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.database.sqlite.SQLiteDatabase;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
 import android.os.Build;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.security.Permission;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
@@ -36,13 +47,15 @@ import java.util.concurrent.ExecutionException;
 public class MainActivity extends AppCompatActivity {
 
     TextView tvScanResults, tvCurrWiFiRSSI;
-    private final int PERMISSIONS_REQUEST_CODE_ACCESS_COARSE_LOCATION = 27;
+    private final int PERMISSIONS_REQUEST_CODE_ACCESS_COARSE_LOCATION = 27,
+            PERMISSIONS_REQUEST_CODE_WRITE_EXTERNAL_STORAGE = 28;
     WifiManager mainWifi;
     WifiReceiver receiverWifi;
     List<ScanResult> wifiList;
     StringBuilder sb = new StringBuilder();
-    private final String TAG = "WIFI";
-    boolean locationPermissionGranted = false;
+    private final String TAG = "WIFI", LOG_TAG = "ERROR";
+    boolean locationPermissionGranted = false,
+            writeExternalStoragePermissionGranted = false;
     boolean wifiScanResultsReceived = true;
     RSSIDistanceDbHelper RssiDistanceDb;
 
@@ -83,10 +96,13 @@ public class MainActivity extends AppCompatActivity {
                 new Thread(new Runnable() {
                     public void run() {
                        String rssi = tvCurrWiFiRSSI.getText().toString();
+                        Log.d("Dialog", "Before Dialog");
                         DialogFragment dialog = new AddValuesToDb();
 //                        TextView rssiValue = (TextView) findViewById(R.id.tvValueRssi);
 //                        rssiValue.setText(rssi);
                         dialog.show(getSupportFragmentManager(),"missiles");
+
+
                         Log.d("Dialog", "After Dialog");
                     }
                 }).start();
@@ -117,6 +133,7 @@ public class MainActivity extends AppCompatActivity {
                         // a potentially  time consuming task
                         //while(true) {
                         Log.d(TAG, "Starting Scan ... ");
+                        //System.out.println("Starting Scan ...");
                         String[] args = {"Wait for Scan Results"};
                         new UpdateGUITask2().execute(args);
 
@@ -139,6 +156,107 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        final Button collectReadings = (Button) findViewById(R.id.buttonStartCollection);
+        collectReadings.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                new Thread(new Runnable() {
+                    public void run() {
+                        try {
+                            final int waitTimeMillis = 3000;
+                            EditText etX = (EditText) findViewById(R.id.etValueXCoord);
+                            EditText etY = (EditText) findViewById(R.id.etValueYCoord);
+
+                            String distance = etX.getText().toString() + "," + etY.getText().toString();
+                            String line = "";
+
+                            for (int i = 0; i < 10; i++) {
+                                Thread.sleep(waitTimeMillis);
+                                WifiInfo wifiInfo = mainWifi.getConnectionInfo();
+                                line += Integer.toString(wifiInfo.getRssi()) + (i < 9 ? "," : "");
+
+                            }
+                            WriteDataToFile(distance + "," + line + "\r\n",
+                                    Integer.toString(waitTimeMillis));
+
+                            ReadingFinishedNotify();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                }).start();
+
+            }
+
+        });
+    }
+
+    public void ReadingFinishedNotify() {
+        //Define Notification Manager
+        NotificationManager notificationManager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+
+//Define sound URI
+        Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        NotificationCompat.Builder mBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(getApplicationContext())
+                .setSmallIcon(R.drawable.ic_search)
+                .setContentTitle("WiFiRssiMapper")
+                .setContentText("Reading Values Finished. Ready for another round!")
+                .setSound(soundUri); //This sets the sound to play
+
+//Display notification
+        notificationManager.notify(0, mBuilder.build());
+    }
+
+    private synchronized void WriteDataToFile(String data, String waitTimeMillis) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSIONS_REQUEST_CODE_WRITE_EXTERNAL_STORAGE);
+            Log.d("INFO", "This is the fuckup!");
+            //After this point you wait for callback in onRequestPermissionsResult(int, String[], int[]) overriden method
+
+        } else {
+            if (isExternalStorageWritable(
+
+            )) {
+                final File path =
+                        Environment.getExternalStoragePublicDirectory
+                                (
+                                        //Environment.DIRECTORY_PICTURES
+                                        Environment.DIRECTORY_PODCASTS + "/RssiData/"
+                                );
+
+//                File file = getAlbumStorageDir(getApplicationContext(), "fileToSave");
+                //Make sure the path directory exists.
+                if (!path.exists()) {
+                    // Make it, if it doesn't exit
+                    path.mkdirs();
+                }
+
+
+                final File file = new File(path, waitTimeMillis + "_values.csv");
+
+                // Save your stream, don't forget to flush() it before closing it.
+
+                try {
+                    FileOutputStream fOut = new FileOutputStream(file, true);
+                    OutputStreamWriter myOutWriter = new OutputStreamWriter(fOut);
+                    myOutWriter.append(data);
+                    myOutWriter.close();
+
+                    fOut.flush();
+                    fOut.close();
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                Log.e(LOG_TAG, "Cannot write to external media!");
+            }
+        }
+
 
     }
 
@@ -151,6 +269,12 @@ public class MainActivity extends AppCompatActivity {
             //mWifiListener.getScanningResults();
             //mainWifi.startScan();
             locationPermissionGranted = true;
+        } else if (requestCode == PERMISSIONS_REQUEST_CODE_WRITE_EXTERNAL_STORAGE
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            // Do something with granted permission
+            //mWifiListener.getScanningResults();
+            //mainWifi.startScan();
+            writeExternalStoragePermissionGranted = true;
         }
     }
 
@@ -158,12 +282,6 @@ public class MainActivity extends AppCompatActivity {
         menu.add(0, 0, 0, "Refresh");
         return super.onCreateOptionsMenu(menu);
     }
-
-//    public boolean onMenuItemSelected(int featureId, MenuItem item) {
-//        mainWifi.startScan();
-//        //mainText.setText("Starting Scan");
-//        return super.onMenuItemSelected(featureId, item);
-//    }
 
     protected void onPause() {
         unregisterReceiver(receiverWifi);
@@ -179,6 +297,37 @@ public class MainActivity extends AppCompatActivity {
 
     public void Auxtask(){
 
+    }
+
+    public File getAlbumStorageDir(Context context, String albumName) {
+        // Get the directory for the app's private pictures directory.
+        File file = new File(context.getExternalFilesDir(Environment.DIRECTORY_PODCASTS), albumName);
+        //String string = Environment.
+        //Log.e(LOG_TAG,string.toString());
+///
+        if (!file.mkdirs()) {
+            Log.e(LOG_TAG, "Directory not created");
+        }
+        return file;
+    }
+
+    /* Checks if external storage is available for read and write */
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    /* Checks if external storage is available to at least read */
+    public boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            return true;
+        }
+        return false;
     }
 
     class WifiReceiver extends BroadcastReceiver {
