b86176709c7421aca3fe928215c374a7e636b0ac
Ritvik <a0229883@ti.com>
2017-04-10 13:27:55 +0530

    Enabling change of color in box. Min API set to 21

--- app/src/main/java/com/example/a0132535/wifirssimapper/MapActivity.java
+++ app/src/main/java/com/example/a0132535/wifirssimapper/MapActivity.java
@@ -1,7 +1,11 @@
 package com.example.a0132535.wifirssimapper;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 /**
  * Created by a0229883 on 05-Apr-17.
@@ -13,5 +17,12 @@ public class MapActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         // Get the view from new_activity.xml
         setContentView(R.layout.activity_map);
+
+        ImageView IV, IV1;
+        IV = (ImageView)findViewById(R.id.Box7);
+        IV.setBackgroundColor(Color.rgb(250, 0, 0));
+
+        IV1 = (ImageView)findViewById(R.id.imageView19);
+        IV1.setBackgroundColor(Color.rgb(0, 250, 0));
     }
 }
---
+++ app/src/main/java/com/example/a0132535/wifirssimapper/ShowCoordinatePosition.java
@@ -0,0 +1,12 @@
+package com.example.a0132535.wifirssimapper;
+
+/**
+ * Created by a0229883 on 10-Apr-17.
+ */
+
+public final class ShowCoordinatePosition {
+    public static void ShoowPosition(double r1, double r2, double r3){
+
+
+    }
+}
